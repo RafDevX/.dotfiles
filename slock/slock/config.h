0ccee6e78a1ce01cc145c630937a52a1bec958cb
Rafael Oliveira <rafdev.x@gmail.com>
2021-06-19 01:50:08 +0100

    add slock

---
+++ slock/slock/config.h
@@ -0,0 +1,15 @@
+/* user and group to drop privileges to */
+static const char *user  = "rafa";
+static const char *group = "rafa";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+static const int timetocancel = 5;
+
+/* APPLIED PATCHES: quickcancel */
