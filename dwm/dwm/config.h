836a2804fb01f10492ef46a0269582c6bbb30bfb
Rafael Oliveira <rafdev.x@gmail.com>
2021-07-08 00:37:46 +0100

    open discord in second monitor

--- dwm/dwm/config.h
+++ dwm/dwm/config.h
@@ -32,7 +32,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Brave",    NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "discord",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "discord",  NULL,       NULL,       1 << 2,       0,           1  },
 	{ "Slack",    NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "Mattermost",NULL,      NULL,       1 << 4,       0,           -1 },
 	{ NULL,       "spotify",  NULL,       1 << 5,       0,           -1 },
