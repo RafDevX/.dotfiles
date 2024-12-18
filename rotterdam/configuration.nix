7fe18e0dc9cd78c1aefd6a3d42a9a248007171b6
Rafael Oliveira <rafdev.x@gmail.com>
2024-12-18 00:54:03 +0000

    enable tool for performing firmware updates

--- rotterdam/configuration.nix
+++ rotterdam/configuration.nix
@@ -13,6 +13,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   hardware.enableRedistributableFirmware = true;
+  services.fwupd.enable = true; # firmware update utility
 
   zramSwap = {
     enable = true;
